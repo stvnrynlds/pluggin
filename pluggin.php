f0b6445562594adc147f974beaa0a94e8e0ee5bd
Steven Reynolds <steven@stevenreynolds.me>
2018-06-09 14:34:20 -0500

    Add author details

--- pluggin.php
+++ pluggin.php
@@ -1,6 +1,11 @@
 <?php 
 /*
-*
-* Plugin Name: Pluggin' 
-*
+Plugin Name:  Pluggin'
+Plugin URI:   https://github.com/stvnrynlds/pluggin
+Description:  Basic WordPress Plugin Header Comment
+Version:      0.0.1
+Author:       Steven Reynolds
+Author URI:   http://stevenreynolds.me
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
